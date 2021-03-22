0ad930ac2a03872ea3fe3e345b0197129c427046
Leonard Crestez <cdleonard@gmail.com>
2021-03-22 23:45:20 +0200

    HACK: force ignore modmagic
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- kernel/module.c
+++ kernel/module.c
@@ -3027,9 +3027,8 @@ static int check_modinfo(struct module *mod, struct load_info *info, int flags)
 		if (err)
 			return err;
 	} else if (!same_magic(modmagic, vermagic, info->index.vers)) {
-		pr_err("%s: version magic '%s' should be '%s'\n",
+		pr_warn("HACK: ignore %s: version magic '%s' should be '%s'\n",
 		       info->name, modmagic, vermagic);
-		return -ENOEXEC;
 	}
 
 	if (!get_modinfo(info, "intree")) {
