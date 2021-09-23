f9ba3a08090e9124d873970cc2133db9da10f5bb
Leonard Crestez <cdleonard@gmail.com>
2021-09-23 18:20:07 +0300

    tcp: authopt: compat fix for missing snmp
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- net/ipv4/tcp_authopt.c
+++ net/ipv4/tcp_authopt.c
@@ -1553,7 +1553,6 @@ int __tcp_authopt_inbound_check(struct sock *sk, struct sk_buff *skb, struct tcp
 	 */
 	if (opt && tcp_parse_md5sig_option(th)) {
 		print_tcpao_notice("TCP AO and MD5 both present on same packet: discarded", skb);
-		NET_INC_STATS(sock_net(sk), LINUX_MIB_TCPAUTHOPTFAILURE);
 		return -EINVAL;
 	}
 #endif
@@ -1563,7 +1562,6 @@ int __tcp_authopt_inbound_check(struct sock *sk, struct sk_buff *skb, struct tcp
 	if (!opt && !key)
 		return 0;
 	if (!opt && key) {
-		NET_INC_STATS(sock_net(sk), LINUX_MIB_TCPAUTHOPTFAILURE);
 		print_tcpao_notice("TCP Authentication Missing", skb);
 		return -EINVAL;
 	}
@@ -1575,7 +1573,6 @@ int __tcp_authopt_inbound_check(struct sock *sk, struct sk_buff *skb, struct tcp
 		 * connections.
 		 */
 		if (info->flags & TCP_AUTHOPT_FLAG_REJECT_UNEXPECTED) {
-			NET_INC_STATS(sock_net(sk), LINUX_MIB_TCPAUTHOPTFAILURE);
 			print_tcpao_notice("TCP Authentication Unexpected: Rejected", skb);
 			return -EINVAL;
 		}
@@ -1592,7 +1589,6 @@ int __tcp_authopt_inbound_check(struct sock *sk, struct sk_buff *skb, struct tcp
 		return err;
 
 	if (memcmp(macbuf, opt->mac, TCP_AUTHOPT_MACLEN)) {
-		NET_INC_STATS(sock_net(sk), LINUX_MIB_TCPAUTHOPTFAILURE);
 		print_tcpao_notice("TCP Authentication Failed", skb);
 		return -EINVAL;
 	}
