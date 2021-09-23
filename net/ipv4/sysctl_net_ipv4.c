2cfc9d6a3d55bac6f7856afcf86dbf17c995cf2b
Leonard Crestez <cdleonard@gmail.com>
2021-09-24 00:43:18 +0300

    tcp: authopt: hardcode sysctl_tcp_authopt = 1
    Not worth dealing with sysctl enabled inside a kpatch
    
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- net/ipv4/sysctl_net_ipv4.c
+++ net/ipv4/sysctl_net_ipv4.c
@@ -27,7 +27,6 @@
 #include <net/ping.h>
 #include <net/protocol.h>
 #include <net/netevent.h>
-#include <net/tcp_authopt.h>
 
 static int two = 2;
 static int four = 4;
@@ -632,17 +631,6 @@ static struct ctl_table ipv4_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_do_static_key,
 	},
-#ifdef CONFIG_TCP_AUTHOPT
-	{
-		.procname	= "tcp_authopt",
-		.data		= &sysctl_tcp_authopt,
-		.maxlen		= sizeof(int),
-		.mode		= 0644,
-		.proc_handler	= proc_tcp_authopt,
-		.extra1		= SYSCTL_ZERO,
-		.extra2		= SYSCTL_ONE,
-	},
-#endif
 	{ }
 };
 
