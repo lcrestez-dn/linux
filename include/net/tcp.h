ce9ea29a1c2fd10a0f01cd3b1743f3b455f2f889
Leonard Crestez <cdleonard@gmail.com>
2021-09-25 23:49:29 +0300

    tcp: use static_key_enabled for &tcp_tx_delay_enabled
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -2300,7 +2300,7 @@ DECLARE_STATIC_KEY_FALSE(tcp_tx_delay_enabled);
 static inline void tcp_add_tx_delay(struct sk_buff *skb,
 				    const struct tcp_sock *tp)
 {
-	if (static_branch_unlikely(&tcp_tx_delay_enabled))
+	if (static_key_enabled(&tcp_tx_delay_enabled))
 		skb->skb_mstamp_ns += (u64)tp->tcp_tx_delay * NSEC_PER_USEC;
 }
 
@@ -2309,7 +2309,7 @@ static inline void tcp_add_tx_delay(struct sk_buff *skb,
  */
 static inline u64 tcp_transmit_time(const struct sock *sk)
 {
-	if (static_branch_unlikely(&tcp_tx_delay_enabled)) {
+	if (static_key_enabled(&tcp_tx_delay_enabled)) {
 		u32 delay = (sk->sk_state == TCP_TIME_WAIT) ?
 			tcp_twsk(sk)->tw_tx_delay : tcp_sk(sk)->tcp_tx_delay;
 
