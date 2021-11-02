5ab4fa06806d1cc183d9f1043db4f6f635360819
Leonard Crestez <cdleonard@gmail.com>
2021-11-02 21:32:20 +0200

    tcp: authopt: Hide interface header ifndef __GENKSYMS__
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- include/uapi/linux/tcp.h
+++ include/uapi/linux/tcp.h
@@ -331,6 +331,8 @@ struct tcp_diag_md5sig {
 	__u8	tcpm_key[TCP_MD5SIG_MAXKEYLEN];
 };
 
+#ifndef __GENKSYMS__
+
 /**
  * enum tcp_authopt_flag - flags for `tcp_authopt.flags`
  */
@@ -465,6 +467,8 @@ struct tcp_authopt_key {
 	 */
 	int	prefixlen;
 };
+
+#endif /* __GENKSYMS__ */
 
 /* setsockopt(fd, IPPROTO_TCP, TCP_ZEROCOPY_RECEIVE, ...) */
 
